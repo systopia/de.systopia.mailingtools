5bfd93ee16820159c2d736f494c44cee09fd943a
Philipp Batroff <batroff@systopia.de>
2018-03-16 15:14:54 +0100

    Added pattern in SQL and JSON format - SMTP Codes (simple, enhanced) - German away patterns

---
+++ resources/update_away_bounce_pattern.sql
@@ -0,0 +1,19 @@
+[
+["2", "nicht im Büro"],
+["2", "[Uu]rlaub"],
+["2", "Account still *legen"],
+["2", "[Mm]ail.*weitergeleitet"],
+["2", "[Ee].*[Mm]ail.*(löschen|umgezogen|abschalten|abgeschaltet|nicht.*gelesen|bearbeitet|nicht.*aktiv)"],
+["2", "(Nachricht.*unzustellbar|unzustellbar.*Nachricht)"],
+["2", "neue.*[Ee].*[Mm]ail"],
+["2", "[Bb]üro.{1,10}erreich"],
+["2", "(nicht|wieder).*erreichbar"],
+["2", "[Aa]utomatische.{1,3}[Aa]ntwort"],
+["2", "out.{1,10}of.{1,10}office"],
+["2", "(nicht.*gelesen|unterwegs)"],
+["2", "unregelmäßig.*Zugang"],
+["2", "automatisch.{1,10}[Nn]achricht"],
+["2", "([Pp]ostfach|[Ee].*[Mm]ail).*(ungültig|nicht.*gültig|gesperrt|gelöscht)"],
+["2", "[Rr]ückmeldung"],
+["2", "im *[Bb]üro"]
+]
