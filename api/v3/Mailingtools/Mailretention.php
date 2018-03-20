d044002825bbf37c1647753e7962a7f3af2bc603
Philipp Batroff <batroff@systopia.de>
2018-03-20 16:10:15 +0100

    Added API function boilerplate code

---
+++ api/v3/Mailingtools/Mailretention.php
@@ -0,0 +1,43 @@
+<?php
+/*-------------------------------------------------------+
+| SYSTOPIA Mailingtools Extension                        |
+| Copyright (C) 2018 SYSTOPIA                            |
+| Author: P. Batroff (batroff@systopia.de)               |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+use CRM_Mailingtools_ExtensionUtil as E;
+
+/**
+ * Mailingtools.Mailretention API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ * @return void
+ * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
+ */
+function _civicrm_api3_mailingtools_Mailretention_spec(&$spec) {
+
+}
+
+/**
+ * Mailingtools.Mailretention API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_mailingtools_Mailretention($params) {
+
+  $mailstore = new CRM_Mailingtools_CheckMailstore();
+  $mailstore->check_mailstore();
+}
