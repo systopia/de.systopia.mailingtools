e5cd658804a19280b10333b0b15bf8c26a66cc72
Philipp Batroff <batroff@systopia.de>
2018-09-21 17:07:57 +0200

    initial emailverifier api command boiler plate code

---
+++ CRM/Mailingtools/EmailVerifier.php
@@ -0,0 +1,38 @@
+<?php
+/*-------------------------------------------------------+
+| SYSTOPIA Mailingtools Extension                        |
+| Copyright (C) 2018 SYSTOPIA                            |
+| Author: P. Batroff (batroff@systopia.de)               |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+
+use CRM_Mailingtools_ExtensionUtil as E;
+
+/**
+ * Class CRM_Mailingtools_EmailVerifier
+ */
+class CRM_Mailingtools_EmailVerifier {
+
+  private $verify_size;
+  private $checking_index;
+  private $debug;
+
+  public function __construct($verify_size, $checking_index, $debug) {
+    $this->verify_size = $verify_size;
+    $this->checking_index = $checking_index;
+    $this->debug = $debug;
+  }
+
+  public function process() {
+    // poll email addresses, and verify
+//    require_once (__DIR__ . '')
+  }
+}
---
+++ api/v3/Mailingtools/Emailsync.php
@@ -0,0 +1,37 @@
+<?php
+use CRM_Mailingtools_ExtensionUtil as E;
+
+/**
+ * Mailingtools.Emailsync API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ * @return void
+ * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
+ */
+function _civicrm_api3_mailingtools_Emailsync_spec(&$spec) {
+  $spec['verify_size']['api.default'] = 1000;
+  $spec['verify_size']['api.description'] = "The number of emails verified.";
+  $spec['checking_index']['api.default'] = 1;
+  $spec['checking_index']['api.description'] = "Uses an email identifier to start checking from there. Id from civicrm_email table is used.";
+  $spec['debug']['api.default'] = FALSE;
+  $spec['debug']['api.description'] = "Writes errors of each lookup to CiviCRM log.";
+}
+
+/**
+ * Mailingtools.Emailsync API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_mailingtools_Emailsync($params) {
+
+  $runner = new CRM_Mailingtools_EmailVerifier($params['verify_size'], $params['checking_index'], $params['debug']);
+  $runner->process();
+  return civicrm_api3_create_success($returnValues, $params, 'NewEntity', 'NewAction');
+
+  throw new API_Exception(/*errorMessage*/ 'Everyone knows that the magicword is "sesame"', /*errorCode*/ 1234);
+}
