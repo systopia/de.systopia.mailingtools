190090812ca7c18f5bbd07674ef755a543d9d06a
Jens Schuppe <schuppe@systopia.de>
2022-07-07 15:23:55 +0200

    Upgrade to Civix 22.05.2 extension structure

--- mailingtools.php
+++ mailingtools.php
@@ -13,15 +13,6 @@ function mailingtools_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function mailingtools_civicrm_xmlMenu(&$files) {
-  _mailingtools_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -75,54 +66,6 @@ function mailingtools_civicrm_disable() {
  */
 function mailingtools_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
   return _mailingtools_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function mailingtools_civicrm_managed(&$entities) {
-  _mailingtools_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function mailingtools_civicrm_caseTypes(&$caseTypes) {
-  _mailingtools_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function mailingtools_civicrm_angularModules(&$angularModules) {
-  _mailingtools_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function mailingtools_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _mailingtools_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
@@ -227,4 +170,13 @@ function mailingtools_civicrm_pageRun(&$page) {
     default:
       return;
   }
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function mailingtools_civicrm_entityTypes(&$entityTypes) {
+  _mailingtools_civix_civicrm_entityTypes($entityTypes);
 }
