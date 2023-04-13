df154f86a32a63dd1082ed31ea7f1c2223924338
jofranz <12087462+jofranz@users.noreply.github.com>
2023-04-13 13:31:50 +0200

    civix upgrade 23.02.1 (#11)
    * [#9] civix upgrade
    
    * civix upgrade 23.02.1
    
    ---------
    
    Co-authored-by: Anna <bugey@systopia.de>

--- mailingtools.php
+++ mailingtools.php
@@ -13,39 +13,12 @@ function mailingtools_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function mailingtools_civicrm_xmlMenu(&$files) {
-  _mailingtools_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
  */
 function mailingtools_civicrm_install() {
   _mailingtools_civix_civicrm_install();
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function mailingtools_civicrm_postInstall() {
-  _mailingtools_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function mailingtools_civicrm_uninstall() {
-  _mailingtools_civix_civicrm_uninstall();
 }
 
 /**
@@ -56,73 +29,6 @@ function mailingtools_civicrm_uninstall() {
 function mailingtools_civicrm_enable() {
   _mailingtools_civix_civicrm_enable();
   CRM_Mailingtools_Config::installScheduledJob();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function mailingtools_civicrm_disable() {
-  _mailingtools_civix_civicrm_disable();
-  // TODO: remove scheduled job!
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function mailingtools_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _mailingtools_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function mailingtools_civicrm_managed(&$entities) {
-  _mailingtools_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function mailingtools_civicrm_caseTypes(&$caseTypes) {
-  _mailingtools_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function mailingtools_civicrm_angularModules(&$angularModules) {
-  _mailingtools_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function mailingtools_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _mailingtools_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
